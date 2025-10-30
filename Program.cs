79f5398b57710e1e627e7b7d59d112ee1be24ac2
Damasus Okeke <gwill2be@gmail.com>
2025-10-30 12:04:29 +0100

    Menu, Room classes and Program.cs improved. User can view all rooms in the hotel

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using App;
 
 
 List<User> users = new();  // List of users
-users.Add(new User("receptionist355", "pass"));
+users.Add(new User("hotell", "pass"));
 
 // save the user to a file (loginInfo.save)
 string[] save_users_to_file = new string[users.Count];
@@ -16,6 +16,20 @@ for (int i = 0; i < users.Count; ++i)
 }
 File.WriteAllLines("loginInfo.save", save_users_to_file);
 
+string filePath = "roomsInfo.save";
+Room[,] hotelRooms = new Room[3, 6]; // 2D array for rooms
+                                     // Initialize rooms
+for (int floorNum = 0; floorNum < 3; floorNum++)
+{
+    for (int roomNum = 0; roomNum < 6; roomNum++)
+    {
+        hotelRooms[floorNum, roomNum] = new Room(floorNum + 1, roomNum + 1);
+    }
+}
+// Load previously saved data
+// LoadSavedRooms(hotelRooms, filePath);
+
+
 User? active_user = null;
 Menu menu = Menu.None;
 
@@ -23,7 +37,7 @@ bool running = true;
 
 while (running)
 {
-    //Console.Clear();
+    Console.Clear();
 
     switch (menu)
     {
@@ -35,7 +49,7 @@ while (running)
                 Console.Write("Enter password: ");
                 string? password = Console.ReadLine();
 
-                //Console.Clear();
+                Console.Clear();
                 Debug.Assert(username != null);
                 Debug.Assert(password != null);
 
@@ -43,7 +57,7 @@ while (running)
                 bool saved_file_found = File.Exists("loginInfo.save");
                 if (!saved_file_found)
                 {
-                    Console.WriteLine("Saved file does not exist");
+                    Console.WriteLine("Saved file does not exist.");
                 }
                 else
                 {
@@ -56,7 +70,7 @@ while (running)
                         if (userInfo[0] == username && userInfo[1] == password)
                         {
                             saved_userInfoMatch = true;
-                            Console.WriteLine($"Welcome! {userInfo[0]}, {userInfo[1]}");
+                            //Console.WriteLine($"Welcome! {userInfo[0]}, {userInfo[1]}");
                             active_user = new User(userInfo[0], userInfo[1]);
                             menu = Menu.Main;
                             break;
@@ -68,29 +82,129 @@ while (running)
                         }
                     }
                 }
-            }break;
+            }
+            break;
 
         case Menu.Main:
             {
-                //Console.Clear();
+                Console.Clear();
                 Console.WriteLine("=====Welcome to the Rooms Tracking System=====");
                 Console.WriteLine("\nChoose what you want to do: ");
-                Console.WriteLine("1. View List of Occcupied Rooms");
-                Console.WriteLine("2. View List of Availble Rooms");
-                Console.WriteLine("3. Book an Available Room");
-                Console.WriteLine("4. Checkout an Occupied Room");
-                Console.WriteLine("5. Mark a Room as In-Service");
+                Console.WriteLine("[V]. View Current Rooms Outlook");
+                Console.WriteLine("[O]. View List of Occcupied Rooms");
+                Console.WriteLine("[A]. View List of Availble Rooms");
+                Console.WriteLine("[B]. Book an Available Room");
+                Console.WriteLine("[C]. Checkout an Occupied Room");
+                Console.WriteLine("[M]. Mark Room as Under Maintenance");
+                Console.WriteLine("[E]. View Event Log");
                 Console.WriteLine("[Q] - Quit");
 
                 switch (Console.ReadLine())
                 {
-                    case "1":
+                    case "V":
+                        {
+                            ViewCurrentRoomsOutlook(hotelRooms);
+                            Console.WriteLine("\nPress ENETER to continue...");
+                            Console.ReadLine();
+                            break;
+                        }
+                    case "O":
+                        {
+                            ViewOccupiedRooms(hotelRooms, RoomStatus.Occupied);
+                            break;
+                        }
+                    case "A":
                         {
 
                         }
                         break;
-                }
-            }break;
-    }        
+                    case "B":
+                        {
 
+                        }
+                        break;
+                    case "C":
+                        {
+
+                        }
+                        break;
+                    case "M":
+                        {
+
+                        }
+                        break;
+                    case "E":
+                        {
+
+                        }
+                        break;
+                    case "Q":
+                        {
+                            running = false;
+                            Console.WriteLine("You have quitted successfully!");
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("Invalid option!");
+                            menu = Menu.Main;
+                        }
+                        break;
+                }
+            }
+            break;
+
+        default:
+            {
+                Console.WriteLine("You are currently logged in!");
+                menu = Menu.Main;
+            }
+            break;
+    }
+}
+static void ViewCurrentRoomsOutlook(Room[,] hotelRooms)
+{
+    Console.Clear();
+    Console.WriteLine("===== Current Hotel Rooms Outlook =====");
+    for (int floorNum = 0; floorNum < hotelRooms.GetLength(0); floorNum++)
+    {
+        Console.Write($"Floor {floorNum + 1}: ");
+        for (int roomNum = 0; roomNum < hotelRooms.GetLength(1); roomNum++)
+        {
+            char mark = hotelRooms[floorNum, roomNum].Status switch
+            {
+                RoomStatus.Available => 'A',
+                RoomStatus.Occupied => 'O',
+                RoomStatus.UnderMaintenance => 'M',
+            };
+            Console.Write($"[{mark}] ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("\nA = Available, O = Occupied, M = UnderMaintenance");
+    Console.WriteLine("___________________________________________________");
+}
+
+static void ViewOccupiedRooms(Room[,] hotelRooms, RoomStatus status)
+{
+    Console.Clear();
+    Console.WriteLine($"===== List of {status} Rooms====");
+    bool found = false;
+    for (int floorNum = 0; floorNum < hotelRooms.GetLength(0); floorNum++)
+    {
+        for (int roomNum = 0; roomNum < hotelRooms.GetLength(1); roomNum++)
+        {
+            if (hotelRooms[floorNum, roomNum].Status == status)
+            {
+                Console.WriteLine($"Floor {hotelRooms[floorNum, roomNum].FloorNumber}, Room {hotelRooms[floorNum, roomNum].RoomNumber}");
+                found = true;
+            }
+        }
+    }
+    if (!found)
+    {
+        Console.WriteLine($"No {status.ToString().ToLower()} rooms found.");
+    }
+    Console.WriteLine("\nPress ENETER to continue...");
+    Console.ReadLine();
 }
--- Room.cs
+++ Room.cs
@@ -4,5 +4,29 @@ enum RoomStatus
 {
     Available,
     Occupied,
-    InService,
+    UnderMaintenance,
+}
+
+class Room
+{
+    public int FloorNumber;
+    public int RoomNumber;
+    public RoomStatus Status;
+
+    public Room(int floorNum, int roomNum)
+    {
+        FloorNumber = floorNum;
+        RoomNumber = roomNum;
+        Status = RoomStatus.Available;
+    }
+
+    public void Occupy()
+    {
+        Status = RoomStatus.Occupied;
+    }
+
+    public void RoomUnderMaintenance()
+    {
+        Status = RoomStatus.UnderMaintenance;
+    }
 }
